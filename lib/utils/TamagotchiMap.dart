29620e12b50fcd6c6916a91fc8e52fa03604a0fd
lay.03(이희진)/kakao <leinaere@gmail.com>
2023-01-29 02:00:26 +0900

    Update TamagotchiMap.dart

--- lib/utils/TamagotchiMap.dart
+++ lib/utils/TamagotchiMap.dart
@@ -1,25 +1,35 @@
 import 'dart:core';
 
 import 'package:flutter/cupertino.dart';
+import 'package:tamahaem/action/CareAction.dart';
+import 'package:tamahaem/action/PlayAction.dart';
+import 'package:tamahaem/event/events/FoundToyEvent.dart';
 import 'package:tamahaem/event/events/HungryEvent.dart';
+import 'package:tamahaem/event/events/PoopEvent.dart';
+import 'package:tamahaem/event/events/SickEvent.dart';
+import 'package:tamahaem/event/events/ThirstyEvent.dart';
 
+import '../action/CleanAction.dart';
 import '../action/FeedAction.dart';
+import '../action/WaterAction.dart';
 
 class TamagotchiMap {
 
   final Map<int, Widget> _actionButtonMap = {
     0: FeedAction(),
-    // 1: "Water",
-    // 2: "Play",
-    // 3: "Clean",
-    // 4: "Care",
-    // 5: "Not impl",
-    // 6: "Not impl"
+    1: WaterAction(),
+    2: PlayAction(),
+    3: CleanAction(),
+    4: CareAction(),
   };
 
   //TODO find out this event is handled.
   final Map<Type, void> _eventHandleMap = {
-    FeedAction: HungryEvent().handleEvent()
+    FeedAction: HungryEvent().handleEvent(),
+    WaterAction: ThirstyEvent().handleEvent(),
+    PlayAction: FoundToyEvent().handleEvent(),
+    CleanAction: PoopEvent().handleEvent(),
+    CareAction: SickEvent().handleEvent()
   };
 
   Map get actionButtonMap => _actionButtonMap;
