7beecf1dd8078d58896ac2b01cc5b1fd8687c2c3
lay.03(이희진)/kakao <leinaere@gmail.com>
2023-01-31 00:18:30 +0900

    default action ui add

--- lib/action/Impl/FeedAction.dart
+++ lib/action/Impl/FeedAction.dart
@@ -1,9 +1,11 @@
 
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:tamahaem/action/AbstractAction.dart';
 import 'package:tamahaem/domain/Tamagotchi.dart';
 import 'package:tamahaem/domain/TamagotchiProvider.dart';
-import 'package:tamahaem/utils/TamagotchiMap.dart';
+
+import '../ui/FeedActionUI.dart';
 
 class FeedAction extends AbstractAction {
   Tamagotchi tamagotchi = TamagotchiProvider().tamagotchi;
@@ -11,16 +13,23 @@ class FeedAction extends AbstractAction {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("돌아가기"),
-          onPressed: () {
-            TamagotchiMap().actionNotifier(runtimeType);
-            Navigator.pop(context);
-          },
-        ),
+      body: SizedBox(
+          width: 0.1,
+          height: 0.1,
+          child: GameWidget(game: FeedActionUI())
       ),
     );
+    // return Scaffold(
+    //   backgroundColor: Colors.blueAccent,
+    //   body: Center(
+    //     child: ElevatedButton(
+    //       child: const Text("돌아가기"),
+    //       onPressed: () {
+    //         TamagotchiMap().actionNotifier(runtimeType);
+    //         Navigator.pop(context);
+    //       },
+    //     ),
+    //   ),
+    // );
   }
 }
