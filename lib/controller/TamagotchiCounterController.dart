5d31b44fee3dc3c89864d53bca27837429d9b7d8
lay.03(이희진)/kakao <leinaere@gmail.com>
2023-01-30 20:08:29 +0900

    Update TamagotchiCounterController.dart

--- lib/controller/TamagotchiCounterController.dart
+++ lib/controller/TamagotchiCounterController.dart
@@ -3,21 +3,20 @@ import 'dart:async';
 import 'package:flutter/animation.dart';
 import 'package:logger/logger.dart';
 import 'package:tamahaem/domain/TamagotchiProvider.dart';
-import 'package:tamahaem/event/EventHandler.dart';
+import 'package:tamahaem/event/EventHandleProvider.dart';
 
 import '../domain/Tamagotchi.dart';
 
 class TamagotchiCounterController {
   late Timer _timer;
-  EventHandler _eventHandler = EventHandler();
   Tamagotchi _tamagotchi = TamagotchiProvider().tamagotchi;
   VoidCallback _onTick;
   Logger logger = Logger();
 
   TamagotchiCounterController(this._onTick) {
     _timer = Timer.periodic(const Duration(hours: 1), _afkPenalty);
-    _timer = Timer.periodic(const Duration(minutes: 1), _defaultPenalty);
-    _timer = Timer.periodic(const Duration(minutes: 1), startEventLoop);
+    _timer = Timer.periodic(const Duration(minutes: 2), _defaultPenalty);
+    _timer = Timer.periodic(const Duration(minutes: 2), startEventLoop);
   }
 
   void _afkPenalty(Timer timer) {
@@ -41,7 +40,7 @@ class TamagotchiCounterController {
   // there is a bug, when expanded button list, then rerendering tamagotchi's movement
   // so, i deleted ontick call in every functions. i dont knwo that is right solution.
   void startEventLoop(Timer timer) {
-    _eventHandler.startEvent();
+    EventHandleProvider().startEvent();
 
     logger.v("event start ${_tamagotchi.hunger} ${_tamagotchi.thirst} ${_tamagotchi.happiness}");
   }
