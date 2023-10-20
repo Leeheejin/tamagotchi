ed05e6c5f2e4104f015cacac4f84869eccce957a
lay.03(이희진)/kakao <lay.03@kakaocorp.com>
2023-10-20 15:00:51 +0900

    [UPDATE] ui정리

--- lib/action/ui/chat/DamagochiChatBox.dart
+++ lib/action/ui/chat/DamagochiChatBox.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class ChatBox extends StatefulWidget {
@@ -17,22 +18,39 @@ class _ChatBoxState extends State<ChatBox> {
     widget.onTap();
   }
 
+  Widget _background(BuildContext context) {
+
+    return const Image(
+      image: AssetImage("assets/images/background/chatBoxBackground.png"),
+      alignment: Alignment.center,
+      height: double.infinity,
+      width: double.infinity,
+      fit: BoxFit.fill,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: _handleTap,
       child:
-      Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.blue),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        padding: EdgeInsets.all(16),
-        child: Text(
-          widget.messages[widget.currentMessageIndex],
-          style: TextStyle(fontSize: 16),
-        ),
-      ),
+          Stack(
+            children: [
+              SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: _background(context)
+              ),
+              Positioned(
+                  top: MediaQuery.of(context).size.height * 0.1,
+                  left: MediaQuery.of(context).size.height * 0.05,
+                  child: Text(
+                    widget.messages[widget.currentMessageIndex],
+                    style: TextStyle(fontSize: 16),
+                  )
+              )
+            ],
+          )
     );
   }
 }
