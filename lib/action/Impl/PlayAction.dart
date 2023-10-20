237597377e6460fae5abac9a6ef628389745d5d7
lay.03(이희진)/kakao <lay.03@kakaocorp.com>
2023-10-20 13:02:38 +0900

    [UPDATE] 임시코드는 수정에 방해가 되므로 더미처리

--- lib/action/Impl/PlayAction.dart
+++ lib/action/Impl/PlayAction.dart
@@ -29,28 +29,16 @@ class _PlayActionState extends State<PlayAction> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: 0.1,
-        height: 0.1,
+      backgroundColor: Colors.blueAccent,
+      body: Center(
         child: ElevatedButton(
-            onPressed: () {
-              TamagotchiMap().actionNotifier(runtimeType);
-            },
-            child: GameWidget(game: FeedActionUI(context: context, currentStep: 4))
+          child: const Text("돌아가기"),
+          onPressed: () {
+            TamagotchiMap().actionNotifier(runtimeType);
+            Navigator.pop(context);
+          },
         ),
       ),
     );
-    // Scaffold(
-    //   backgroundColor: Colors.blueAccent,
-    //   body: Center(
-    //     child: ElevatedButton(
-    //       child: const Text("돌아가기"),
-    //       onPressed: () {
-    //         TamagotchiMap().actionNotifier(runtimeType);
-    //         Navigator.pop(context);
-    //       },
-    //     ),
-    //   ),
-    // );
   }
 }
